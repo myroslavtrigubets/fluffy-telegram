33972c512aecc35aefdf048526a041d7cff3ddfc
Myroslav Trigubets <myroslav.trigubets@gmail.com>
2017-10-31 19:17:44 +0200

    update all files

---
+++ main.py
@@ -0,0 +1,26 @@
+import config
+import telebot
+import os
+
+bot = telebot.TeleBot(config.token)
+
+@bot.message_handler(content_types=["text"])
+def repeat_all_messages(message): 
+	if message.text == "/start": 
+		bot.send_message(message.chat.id, "message.text")
+
+	elif message.text == "uptime":  
+		f = os.popen('uptime').read()
+		bot.send_message(message.chat.id, f)
+
+	elif "/ssh" in message.text: 
+		ssh = message.text.replace("/ssh", "") 
+		f = os.popen(ssh).read()
+		bot.send_message(message.chat.id, f)
+		
+	else:
+		bot.send_message(message.chat.id, message.text)
+
+if __name__ == '__main__':
+     bot.polling(none_stop=True)
+
