aeadd068f5898530c0840f0fee86fcaaee07cc33
Myroslav Trigubets <myroslav.trigubets@gmail.com>
2017-11-02 11:19:04 +0200

    Add admins control

--- main.py
+++ main.py
@@ -5,21 +5,22 @@ import os
 bot = telebot.TeleBot(config.token)
 
 @bot.message_handler(content_types=["text"])
-def repeat_all_messages(message): 
-	if message.text == "/start": 
-		bot.send_message(message.chat.id, "message.text")
+def commands(message): 
+	if message.chat.id in config.admins:
+		if message.text == "/start": 
+			bot.send_message(message.chat.id, "message.text")
 
-	elif "uptime" in message.text:   
-		f = os.popen('uptime').read()
-		bot.send_message(message.chat.id, f)
+		elif "uptime" in message.text:   
+			f = os.popen('uptime').read()
+			bot.send_message(message.chat.id, f)
 
-	elif "/ssh" in message.text: 
-		ssh = message.text.replace("/ssh", "") 
-		f = os.popen(ssh).read()
-		bot.send_message(message.chat.id, f)
+		elif "/ssh" in message.text: 
+			ssh = message.text.replace("/ssh", "") 
+			f = os.popen(ssh).read()
+			bot.send_message(message.chat.id, f)
 
-	else:
-		bot.send_message(message.chat.id, message.text)
+		else:
+			bot.send_message(message.chat.id, message.text)
 
 if __name__ == '__main__':
      bot.polling(none_stop=True)
