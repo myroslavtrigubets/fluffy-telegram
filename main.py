7efd0212a3635e7e2caab11af0753989105c0a1b
Myroslav Trigubets <myroslav.trigubets@gmail.com>
2017-11-04 23:53:15 +0200

    Updates bot functions

--- main.py
+++ main.py
@@ -20,9 +20,14 @@ def commands(message):
 			f = os.popen(ssh).read()
 			bot.send_message(message.chat.id, f)
 
+		elif "/photo" in message.text: 
+
+			bot.send_message(message.chat.id, "Send Photo")
+
+			bot.send_photo(message.chat.id, open('/root/fluffy-telegram/test.png', 'rb'))
+
 		else:
 			bot.send_message(message.chat.id, message.text)
 
 if __name__ == '__main__':
      bot.polling(none_stop=True)
-
